9778e528982aacc03e16ad9e4c6bfa8ac0774574
Aspira GT <aspiragt@gmail.com>
2025-02-10 20:20:04 -0600

    fix: Update port configuration for Render

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import uvicorn
+import os
 from app.main import app
 
 # Esta línea es necesaria para que Render encuentre la aplicación
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=port,
+        reload=True
+    )
